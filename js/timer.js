ba68b1a491d2a5098a8c44c9a2cfe53fbe8f760e
belieflab <joshuagkenney@gmail.com>
2020-06-10 12:26:54 -0400

    timer working, but starting too early

--- js/timer.js
+++ js/timer.js
@@ -1,22 +1,31 @@
 function countdown(minutes) {
     var seconds = 60;
-    var mins = minutes
+    var mins = minutes;
     function tick() {
         //This script expects an element with an ID = "counter". You can change that to what ever you want. 
-        var counter = document.getElementById("counter");
+        // var counter = document.getElementById("counter");
+        var counter = '';
         var current_minutes = mins-1
         seconds--;
-        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        counter = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
         if( seconds > 0 ) {
             setTimeout(tick, 1000);
+            console.log(seconds);
         } else {
             if(mins > 1){
                 countdown(mins-1);           
             }
+            else if (seconds == 0) {
+                jsPsych.endCurrentTimeline();
+            }
         }
     }
     tick();
-    if (seconds == 0) {
-        jsPsych.endCurrentTimeline;
-    }
+
 }
+
+
+// if (seconds == 0) {
+//     alert("hello");
+// }
+
