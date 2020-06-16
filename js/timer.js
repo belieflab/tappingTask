07f69b060c4db119018fee0cff2a0cd4e9ca8a01
kjosborne <juston.osborne@u.northwestern.edu>
2020-06-16 17:15:01 -0500

    finished adding blocks, hid timer, added final procedure, looking good

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,6 @@
 function countdown(minutes) {
     // jsPsych.endCurrentTimeline();
-    let seconds = 60;
+    let seconds = 10;
     let mins = minutes;
     function tick() {
         //This script expects an element with an ID = "counter". You can change that to what ever you want. 
@@ -18,6 +18,7 @@ function countdown(minutes) {
             }
             else if (seconds == 0) { //ends experiment when timer reaches 0
                 jsPsych.endCurrentTimeline();
+               // seconds = 10;
             }
         }
     }
@@ -25,7 +26,33 @@ function countdown(minutes) {
 
 }
 
+function countdown2(minutes) {
+    // jsPsych.endCurrentTimeline();
+    let seconds = 10;
+    let mins = minutes;
+    function tick() {
+        //This script expects an element with an ID = "counter". You can change that to what ever you want. 
+        let counter = document.getElementById("counter");
+        // let counter = '';
+        let current_minutes = mins-1
+        seconds--;
+        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds); //comment out .innerHTML method to hide the timer
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+            console.log(seconds);
+        } else {
+            if(mins > 1){
+                countdown(mins-1);           
+            }
+            else if (seconds == 0) { //ends experiment when timer reaches 0
+                jsPsych.endCurrentTimeline();
+               // seconds = 10;
+            }
+        }
+    }
+    tick();
 
+}
 
 // var input = document.getElementById("myInput");
 // input.addEventListener("keyup", function(event) {
