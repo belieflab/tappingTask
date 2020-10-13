abdfce530bb20c9771618040553af72848700711
Joshua Kenney <joshuagkenney@gmail.com>
2020-10-13 18:43:40 -0400

    areYouSure()

--- exp/fn.js
+++ exp/fn.js
@@ -13,3 +13,9 @@ function startExperiment(){
     show_progress_bar: true,
   });
 }
+
+  //onbeforeunload in body
+  function areYouSure() {
+    return "Write something clever here...";
+  }
+  areYouSure();
