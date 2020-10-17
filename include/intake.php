1e30722e334c9bc66ae4351af428d212939af5b3
Joshua Kenney <joshuagkenney@gmail.com>
2020-10-16 21:10:21 -0400

    cursor shows and hides, areYouReady() unloading

--- include/intake.php
+++ include/intake.php
@@ -76,10 +76,8 @@
 </div>
 <script type="text/javascript" src="//code.jquery.com/jquery-git.js"></script>
 <script>$("button.loadMain").click(function(){
-      // $.getScript("exp/timeline.js");
     $.getScript("exp/main.js");
-    // $.getScript("exp/timeline.js");
-    // $.load("include/consent.php");
+    $("body").addClass("hideCursor");
   }); </script>
 <script>
 </script>
