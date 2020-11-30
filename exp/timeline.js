e7bf536d495fc8c1973d63cc936ab9e40a238d45
Joshua Kenney <joshuagkenney@gmail.com>
2020-11-30 05:04:39 -0500

    changed saveData to taskAlias

--- exp/timeline.js
+++ exp/timeline.js
@@ -264,7 +264,7 @@ let save_data = {
   choices: jsPsych.NO_KEYS,
   trial_duration: 5000,
   on_finish: function(){
-    saveData("tempo-tapping_" + workerId, jsPsych.data.get().csv());
+    saveData("tempo-tap_" + workerId, jsPsych.data.get().csv());
     document.getElementById("unload").onbeforeunload='';
     $(document).ready(function(){
     $("body").addClass("showCursor"); // returns cursor functionality
