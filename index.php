8960392d132fd0cb09091b347b9ae39f27c026a2
joshkenney <joshuagkenney@gmail.com>
2020-12-31 16:26:19 -0500

    integrated omnibus queryString

--- index.php
+++ index.php
@@ -20,13 +20,18 @@ if (isset($candidateId)) {
   $prepare->close();
   } else {
 }
+$subjectKey = $_GET["subjectkey"];
+$consortId = $_GET["src_subject_id"];
+$sexAtBirth = $_GET["sex"];
+$institutionAlias = $_GET["site"];
+$ageInMonths = $_GET["interview_age"];
 ?>
 
 <!DOCTYPE html>
 <html>
   <head>
     <title>Tapping Task</title>  <!-- This is for changing the title -->
-    <script src="db/submit.js"></script>
+    <script src="db/validate.js"></script>
     <script src="jsPsych/jspsych.js"></script>
     <script src="jsPsych/plugins/jspsych-audio-keyboard-response.js"></script>
     <script src="jsPsych/plugins/jspsych-html-keyboard-response.js"></script> <!--script is for javascript -->
@@ -36,7 +41,7 @@ if (isset($candidateId)) {
     <link rel="stylesheet" type="text/css" href="css/style.css"> <!--the interpreter will take care of ordering, rel, type, href do not have to be in a specific order -->
   </head>
   <body id='unload' onbeforeunload="return areYouSure()" style="background-color:gray;">  <!--any time you see style = all properties that follow are inline css -->
-    <?php include_once "include/intake.php"?>
+    <?php include_once "include/nda.php"?>
   </body>
 <footer>
   <script src="exp/jQuery.js"></script>
@@ -44,6 +49,11 @@ if (isset($candidateId)) {
   <script src="exp/var.js"></script>
   <script type="text/javascript">
     let feedbackLink = "https://belieflab.yale.edu/omnibus/eCRFs/feedback/tasks/var-tap.php?candidateId=<?php echo $candidateId?>&studyId=<?php echo $studyId?>";
+    let GUID = "<?php echo $subjectKey?>";
+    let subjectID = "<?php echo $consortId?>";
+    let sexAtBirth = "<?php echo $sexAtBirth?>";
+    let siteNumber = "<?php echo $institutionAlias?>";
+    let ageAtAssessment = "<?php echo $ageInMonths?>";
     </script>
 </footer>
 </html>
