85ed98a33641773c0c2b42e0d01316b87570bd8c
joshkenney <joshuagkenney@gmail.com>
2021-01-07 21:12:04 -0500

    fixed bug where antihandnedness was not defined

--- db/validate.js
+++ db/validate.js
@@ -100,8 +100,10 @@ function submitIntake() {
 
     if (rightHandedness === true) {
         handedness = "right";
+        antihandedness = "left";
     } else if (leftHandedness === true) {
         handedness = "left";
+        antihandedness = "right";
     }
 
     // const zeroPad = (num, places) => String(num).padStart(places, '0');
